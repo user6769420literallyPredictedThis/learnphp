8bf4f36babb6460f3ecb59a56e508103f972e215
Uljana Jegorova <uljana.jegorova@techno.ee>
2026-09-04 14:06:31 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
-$test = true;
-$test = false;
-$test = true && false; // false
-$test = false && false; // false
-$test = true && true; // true
-$test = true || true; // true
-$test = true || false; // true alt+ctrl "<>" for "|"
-$test = false || false; // false
-$test = !false; // true, not false
-$test = !true; // false, not true
-$test = 10 > 5; // true, 10 is greater than 5
-$test = !(10 > 5); // false, not true
-$test = 100 == 100; // true, 100 is equal to 100
-$test = 100 >= 100; // true, 100 is greater than or equal to 100
-$test = 100 != 100; // false, 100 is not equal to 100
-$test = 100 !== 100; // false, 100 is not equal to 100, strictly
-$test = 100 == '100'; // true, 100 is equal to 100
-$test = 100 === '100'; // true, 100 is equal to 100, strictly
-$test = true && false || (true && !true); // true and false is false, true and not true is false, false or false is false
-var_dump($test);
+$test = [1, 2, 'word', true];
+var_dump($test[3]); // Prints the 4th element of the array, which is true
+$test[2] = 'new word'; // Changes the 3rd element of the array to 'new word'
+$test[4] = 'new value'; // Adds a new element to the array at index 4
+$test[] = 'another value'; // Adds a new element to the end of the array
+array_push($test, 'pushed value'); // Adds a new element to the end of the array using array_push. Same as last example but more complicated..
+$test = [ 
+    'name' => 'John',
+    'age' => 30,
+    'city' => 'New York',
+    'random key',
+    100 => 'New value',
+    ]; // Reassigns the array to a new associative array
+var_dump($test['name']); // Prints the value of the 'name' key, which is 'John'
+var_dump($test); // Prints the value of the 100 key, which is 'New value'
+
+$test = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+var_dump($test[1][2]); // Prints the value of the 3rd element of the 2nd array, which is 6
 ?>
