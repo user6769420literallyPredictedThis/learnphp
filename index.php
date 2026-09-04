1a3f39c96d48909e335b1cab17e5c1b926da6ba4
Uljana Jegorova <uljana.jegorova@techno.ee>
2026-09-04 13:50:47 +0300

    Add boolean math

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
-$test = 'Hello, World!';
-$test = 'This is a ' . 'test string';
-$test .= '!';
-$name = 'John Doe';
-$age = 30;
-$test = $name . ' is ' . $age . ' years old.';
-$test = "$name is $age years old."; // Double-quoted strings allow to connect variables directly
-$test = "Hello\n\nWorld!"; // Double-quoted strings allow to connect variables directly
-$letter = 'A';
-$letter ++;
-
-var_dump($letter);
-
-// https://www.php.net/manual/en/ref.strings.php String functions.
+$test = true;
+$test = false;
+$test = true && false; // false
+$test = false && false; // false
+$test = true && true; // true
+$test = true || true; // true
+$test = true || false; // true alt+ctrl "<>" for "|"
+$test = false || false; // false
+$test = !false; // true, not false
+$test = !true; // false, not true
+$test = 10 > 5; // true, 10 is greater than 5
+$test = !(10 > 5); // false, not true
+$test = 100 == 100; // true, 100 is equal to 100
+$test = 100 >= 100; // true, 100 is greater than or equal to 100
+$test = 100 != 100; // false, 100 is not equal to 100
+$test = 100 !== 100; // false, 100 is not equal to 100, strictly
+$test = 100 == '100'; // true, 100 is equal to 100
+$test = 100 === '100'; // true, 100 is equal to 100, strictly
+$test = true && false || (true && !true); // true and false is false, true and not true is false, false or false is false
+var_dump($test);
 ?>
