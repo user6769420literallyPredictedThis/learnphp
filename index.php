4a9f2bb35c9e5a5273878faea232c71f7d2627bf
Uljana Jegorova <uljana.jegorova@techno.ee>
2026-09-11 13:30:37 +0300

    Add magic method examples

--- index.php
+++ index.php
@@ -1,25 +1,58 @@
 <?php
 
-class Cat {
-    use makeSound; // Using the trait
-}
+class Box {
 
-class Dog {
-    use hasSmell;
-}
-
-trait hasSmell {
-    public $smell;
-    public function sniff() {
-        return "Smells like $this->smell";
+    public function __construct(public $w, private $h, private $l) {
+        var_dump('Box was created!');
     }
+
+    public function volume() {
+        return $this->w * $this->h * $this->l;
+    }
+
+    public function __set($name, $value) {
+        var_dump($name, $value);
+    }
+
+    public function __get($name) {
+        var_dump($name);
+        return 'YOLO';
+    }
+
+    public function __invoke() {
+        var_dump('Im a function!');
+    }
+
+    public function __call($name, $args) {
+        var_dump($name, $args);
+    }
+
+    public function __toString() {
+        return "Im a box yay!";
+    }
+
+    public function __destruct() {
+        var_dump('Box was destroyed!');
+    }
+
 }
 
-trait makeSound { // Trait to add sound functionality
-    public $sound;
-    public function noise() {
-        return $this->sound;
-    }
+function lol(){
+    $box4 = new Box(4, 4, 5);
 }
+
+lol();
+
+$box1 = new Box(1, 2, 3);
+$box1->color = 'hello';
+$box1();
+$box1->hello(1, 'asdasdasd', 'dfgdfgdfg');
+var_dump($box1->lolololol);
+$box2 = new Box(4, 5, 6);
+var_dump($box1, $box2);
+$box3 = clone $box2;
+echo $box1;
+
+// https://www.php.net/manual/en/language.oop5.magic.php magic methods
 
 ?>
