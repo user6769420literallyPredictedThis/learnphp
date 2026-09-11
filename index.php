7c6cf71d7ce7a68f788a97370273360154668a32
Uljana Jegorova <uljana.jegorova@techno.ee>
2026-09-11 13:47:36 +0300

    Add static examples

--- index.php
+++ index.php
@@ -2,57 +2,32 @@
 
 class Box {
 
-    public function __construct(public $w, private $h, private $l) {
-        var_dump('Box was created!');
+    public static $count = 0;
+    public function __construct(private $w, private $h, private $l) {
+        self::$count++;
+
     }
 
     public function volume() {
         return $this->w * $this->h * $this->l;
     }
 
-    public function __set($name, $value) {
-        var_dump($name, $value);
-    }
-
-    public function __get($name) {
-        var_dump($name);
-        return 'YOLO';
-    }
-
-    public function __invoke() {
-        var_dump('Im a function!');
-    }
-
-    public function __call($name, $args) {
-        var_dump($name, $args);
-    }
-
-    public function __toString() {
-        return "Im a box yay!";
-    }
-
-    public function __destruct() {
-        var_dump('Box was destroyed!');
+    public static function me(){
+        var_dump(self::class);
+        var_dump(static::class);
     }
 
 }
 
-function lol(){
-    $box4 = new Box(4, 4, 5);
+class MetalBox extends Box {
+
 }
 
-lol();
+Box::$count = 1;
+Box::$count = 2;
+Box::me();
+MetalBox::me();
+var_dump(Box::$count, Box::$count);
 
-$box1 = new Box(1, 2, 3);
-$box1->color = 'hello';
-$box1();
-$box1->hello(1, 'asdasdasd', 'dfgdfgdfg');
-var_dump($box1->lolololol);
-$box2 = new Box(4, 5, 6);
-var_dump($box1, $box2);
-$box3 = clone $box2;
-echo $box1;
-
-// https://www.php.net/manual/en/language.oop5.magic.php magic methods
 
 ?>
