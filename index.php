73123dedbb7021f44f6f5a430ac26682bfe404ae
Uljana Jegorova <uljana.jegorova@techno.ee>
2026-09-11 14:49:06 +0300

    Add php templating example

--- index.php
+++ index.php
@@ -1,44 +1,28 @@
 <?php
-
-// library
-
-class Job {
-    public function task(Logger $logger) {
-        for($i=0;$i<10;$i++){
-            //some work is done (video processing or wtv)
-            $logger->log("Task $i was done!");
-        }
-    }
-}
-
-class ConsoleLogger implements Logger {
-    public function log($message) {
-
-    }
-}
-
-interface Logger {
-    public function log($message);
-}
-
-// user code 
-
-class FileLogger implements Logger {
-    public function log($message) {
-        $file = fopen('log.txt', 'a');
-        fwrite($file, "$message\n");
-        fclose($file);
-    }
-}
-
-class  NothingLogger {
-    public function log($message) {
-
-    }
-}
-
-$job = new Job;
-$logger = new FileLogger();
-$job->task($logger);
-
+$name = 'John';
+$coinFlip = rand(0,1);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?=$name?>!</h1>
+    <?php if($coinFlip): ?>
+    <h1>Kull</h1>
+    <?php else: ?>
+    <h1>Kiri</h1>
+    <?php endif; ?>
+
+<ul>
+    <?php for($i=0;$i<10;$i++): ?>
+        <li><?=$i?></li>
+        <?php endfor ?>
+</ul>
+
+</body>
+</html>
