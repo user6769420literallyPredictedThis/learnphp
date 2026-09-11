62cac9f3919e3c1b247370fa5361a5fd100cef46
Uljana Jegorova <uljana.jegorova@techno.ee>
2026-09-11 15:56:02 +0300

    Some sort of program to calculate something idk

--- index.php
+++ index.php
@@ -1,28 +1,8 @@
 <?php
-$name = 'John';
-$coinFlip = rand(0,1);
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello <?=$name?>!</h1>
-    <?php if($coinFlip): ?>
-    <h1>Kull</h1>
-    <?php else: ?>
-    <h1>Kiri</h1>
-    <?php endif; ?>
-
-<ul>
-    <?php for($i=0;$i<10;$i++): ?>
-        <li><?=$i?></li>
-        <?php endfor ?>
-</ul>
-
-</body>
-</html>
+$sum = 0;
+for($i=1;$i<1000;$i++) {
+    if($i%3 === 0 || $i%5 ===0) {
+        $sum+=$i;
+    }
+}
+var_dump($sum);
