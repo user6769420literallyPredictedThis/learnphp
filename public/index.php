82d46bf0c069b7886af682eb42a26836b89858b6
Uljana Jegorova <uljana.jegorova@techno.ee>
2026-09-16 09:30:43 +0300

    Add autoload and namespaces

--- public/index.php
+++ public/index.php
@@ -9,46 +9,63 @@ function dump(...$vars)
     echo '<pre>';
 }
 
-switch ($_SERVER['REQUEST_URI']) {
-    case '/':
-        $title = 'World';
-        $posts = [
-            [
-                'title' => 'Some world title 1',
-                'content' => 'Some world content 1',
-                'date' => 'January 1, 2021',
-                'author' => 'Pets',
-            ],
+spl_autoload_register(function ($class) {
+    $class = substr($class, 4);
+    $class = str_replace('\\', '/', $class);
+    require_once __DIR__ . "/../src/$class.php";
 
-            [
-                'title' => 'Some world title 2',
-                'content' => 'Some world content 2',
-                'date' => 'January 2, 2021',
-                'author' => 'Juss',
-            ],
+});
 
-            [
-                'title' => 'Some world title 3',
-                'content' => 'Some world content 3',
-                'date' => 'January 3, 2021',
-                'author' => 'Alex',
-            ],
+use App\Controllers\PublicController as PC; //creates an alias for App\Controllers\PublicController.
 
-            [
-                'title' => 'Some world title 4',
-                'content' => 'Some world content 4',
-                'date' => 'January 4, 2021',
-                'author' => 'Manivald',
-            ],
-        ];
-        include __DIR__ . '/../views/index.php';
-        break;
-    case '/us':
-        include __DIR__ . '/../views/us.php';
-        break;
-    case '/tech':
-        include __DIR__ . '/../views/tech.php';
-        break;
-    default:
-        echo '404';
-}
+// require_once __DIR__ . '/../src/Router.php';
+// require_once __DIR__ . '/../src/DB.php';
+
+$controller = new PC();
+$router = new App\Router();
+$db = new App\DB();
+dump($router, $db);
+
+// switch ($_SERVER['REQUEST_URI']) {
+//     case '/':
+//         $title = 'World';
+//         $posts = [
+//             [
+//                 'title' => 'Some world title 1',
+//                 'content' => 'Some world content 1',
+//                 'date' => 'January 1, 2021',
+//                 'author' => 'Pets',
+//             ],
+
+//             [
+//                 'title' => 'Some world title 2',
+//                 'content' => 'Some world content 2',
+//                 'date' => 'January 2, 2021',
+//                 'author' => 'Juss',
+//             ],
+
+//             [
+//                 'title' => 'Some world title 3',
+//                 'content' => 'Some world content 3',
+//                 'date' => 'January 3, 2021',
+//                 'author' => 'Alex',
+//             ],
+
+//             [
+//                 'title' => 'Some world title 4',
+//                 'content' => 'Some world content 4',
+//                 'date' => 'January 4, 2021',
+//                 'author' => 'Manivald',
+//             ],
+//         ];
+//         include __DIR__ . '/../views/index.php';
+//         break;
+//     case '/us':
+//         include __DIR__ . '/../views/us.php';
+//         break;
+//     case '/tech':
+//         include __DIR__ . '/../views/tech.php';
+//         break;
+//     default:
+//         echo '404';
+// }
